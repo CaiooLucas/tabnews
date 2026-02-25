391db26309b92ab62444e3681d16a57b23d45ab4
CaiooLucas <caio.borges210805@gmail.com>
2026-02-25 00:03:46 -0300

    Alteração do texto da landingpage v1.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>Eu amo você!</h1>;
+  return (
+    <>
+      <h1>Bem vindo ao meu primeiro site com um dominio registrado!</h1>
+      <p>
+        Se você for um dos casas, quem leu é gay! (Regras do MEU SITE HAHAHAHA)
+      </p>
+    </>
+  );
 }
 
 export default Home;
