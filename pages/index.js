67994fe96df152ef3bd04c881b3c8851e322f14c
CaiooLucas <caio.borges210805@gmail.com>
2026-08-09 05:23:57 -0300

    voltando ao tabnews normal

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <>
+      <h1>Bem vindo ao meu primeiro site com um dominio registrado!</h1>
+      <p>
+        Se você for um dos meus amigos, quem leu é gay! (Regras do MEU SITE
+        HAHAHAHA)
+      </p>
+      <button
+        onClick={() =>
+          alert(
+            "A mensagem do site verdadeira pra você é: Você é muito especial para mim!",
+            "Se você abriu isso, lamento informar mas além de curioso você é corno.",
+          )
+        }
+      >
+        Se você for meu pai, clique aqui!
+        NÃO CLIQUE NESSE BOTÃO
+      </button>
+    </>
+  );
+}
+
+export default Home;
