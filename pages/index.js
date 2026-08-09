812a6b5d6285a5598c95e7028830e51871bf3ab0
CaiooLucas <caio.borges210805@gmail.com>
2026-08-09 05:25:54 -0300

    atualizando o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
 function Home() {
   return (
     <>
-      <h1>Bem vindo ao meu primeiro site com um dominio registrado!</h1>
+      <h1>Bem vindo ao meu lado obscuro do meu dominio</h1>
       <p>
-        Se você for um dos meus amigos, quem leu é gay! (Regras do MEU SITE
-        HAHAHAHA)
+        Não era para você estar aqui pq é aqui que eu testo meus conhecimentos.
       </p>
-      <button
-        onClick={() =>
-          alert(
-            "A mensagem do site verdadeira pra você é: Você é muito especial para mim!",
-            "Se você abriu isso, lamento informar mas além de curioso você é corno.",
-          )
-        }
-      >
-        Se você for meu pai, clique aqui!
-        NÃO CLIQUE NESSE BOTÃO
-      </button>
+      <p>Não leve nada que ver aqui em consideração! Estou estudando.</p>
     </>
   );
 }
