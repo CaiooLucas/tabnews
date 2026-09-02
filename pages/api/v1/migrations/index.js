7b459855ee124f6e23354a7faabb6e688d4094c3
caionascimento-BRPRIME <caio.nascimento@brprime.com.br>
2026-09-02 12:07:46 -0300

    fix json format to accept concat

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,7 +6,7 @@ export default async function status(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
-      error: 'Method "${request.method}" not allowed',
+      error: `Method "${request.method}" not allowed`,
     });
   }
   let dbClient;
@@ -35,7 +35,7 @@ export default async function status(request, response) {
       }
       response.status(200).json(migratedMigrations);
     }
-    
+
   } catch (error) {
     console.error(error)
     throw error;
